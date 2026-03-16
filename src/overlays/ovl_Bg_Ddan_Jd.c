11ec0c76d75f6255f63a7d8418c4d7bcff3bc2f6
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-16 18:39:49 +0100

    BgDdanJd docs

--- src/overlays/ovl_Bg_Ddan_Jd.c
+++ src/overlays/ovl_Bg_Ddan_Jd.c
@@ -15,9 +15,9 @@ void BgDdanJd_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgDdanJd_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgDdanJd_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-void func_80870B88(BgDdanJd* this, GlobalContext* globalCtx);
+void BgDdanJd_Idle(BgDdanJd* this, GlobalContext* globalCtx);
 void func_80870D2C(BgDdanJd* this, GlobalContext* globalCtx);
-void func_80870F00(BgDdanJd* this, GlobalContext* globalCtx);
+void BgDdanJd_Move(BgDdanJd* this, GlobalContext* globalCtx);
 
 extern Gfx D_60037B8;
 extern UNK_TYPE D_6003CE0;
@@ -48,14 +48,14 @@ void BgDdanJd_Init(Actor* thisx, GlobalContext* globalCtx) {
     DynaPolyInfo_SetActorMove(&this->dyna, DPM_PLAYER);
     DynaPolyInfo_Alloc(&D_6003CE0, &sp24);
     this->dyna.dynaPolyId = DynaPolyInfo_RegisterActor(globalCtx, &globalCtx->colCtx.dyna, &this->dyna.actor, sp24);
-    this->unk16A = 0x64;
-    this->unk168 = 0;
+    this->timer = 100;
+    this->state = 0;
     if (Flags_GetSwitch(globalCtx, this->dyna.actor.params)) {
-        this->unk169 = 5;
+        this->yVelocity = 5;
     } else {
-        this->unk169 = 1;
+        this->yVelocity = 1;
     }
-    this->actionFunc = func_80870B88;
+    this->actionFunc = BgDdanJd_Idle;
 }
 
 void BgDdanJd_Destroy(Actor* thisx, GlobalContext* globalCtx) {
@@ -64,45 +64,44 @@ void BgDdanJd_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
 }
 
-void func_80870B88(BgDdanJd* this, GlobalContext* globalCtx) {
-    if (this->unk16A != 0) {
-        this->unk16A--;
+void BgDdanJd_Idle(BgDdanJd* this, GlobalContext* globalCtx) {
+    if (this->timer != 0) {
+        this->timer--;
     }
-    if (this->unk169 == 1) {
-        if ((this->dyna.actor.params < 0x40) && (Flags_GetSwitch(globalCtx, this->dyna.actor.params))) {
-            this->unk169 = 5;
-            this->unk168 = 1;
-            this->unk16A = 0;
-            this->dyna.actor.posRot.pos.y = this->dyna.actor.initPosRot.pos.y + 140.0f;
-            func_800800F8(globalCtx, 0xBF4, -0x63, &this->dyna.actor, 0);
-        }
+    if ((this->yVelocity == 1) && (this->dyna.actor.params < 0x40) &&
+        Flags_GetSwitch(globalCtx, this->dyna.actor.params)) {
+        this->yVelocity = 5;
+        this->state = 1;
+        this->timer = 0;
+        this->dyna.actor.posRot.pos.y = this->dyna.actor.initPosRot.pos.y + 140.0f;
+        func_800800F8(globalCtx, 0xBF4, -0x63, &this->dyna.actor, 0);
     }
-    if (this->unk16A == 0) {
-        this->unk16A = 0x64;
-        if (this->unk168 == 0) {
-            this->unk168 = 1;
-            this->unk16C = this->dyna.actor.initPosRot.pos.y + 140.0f;
-        } else if (this->unk168 == 1) {
-            if (this->unk169 != 1) {
-                this->unk168 = 3;
-                this->unk16C = this->dyna.actor.initPosRot.pos.y + 700.0f;
+    if (this->timer == 0) {
+        this->timer = 100;
+        if (this->state == 0) {
+            this->state = 1;
+            this->targetPosY = this->dyna.actor.initPosRot.pos.y + 140.0f;
+        } else if (this->state == 1) {
+            if (this->yVelocity != 1) {
+                this->state = 3;
+                this->targetPosY = this->dyna.actor.initPosRot.pos.y + 700.0f;
             } else {
-                this->unk168 = 0;
-                this->unk16C = this->dyna.actor.initPosRot.pos.y;
+                this->state = 0;
+                this->targetPosY = this->dyna.actor.initPosRot.pos.y;
             }
-        } else if (this->unk168 == 2) {
-            if (this->unk169 != 1) {
-                this->unk168 = 3;
-                this->unk16C = this->dyna.actor.initPosRot.pos.y + 700.0f;
+        } else if (this->state == 2) {
+            if (this->yVelocity != 1) {
+                this->state = 3;
+                this->targetPosY = this->dyna.actor.initPosRot.pos.y + 700.0f;
             } else {
-                this->unk168 = 0;
-                this->unk16C = this->dyna.actor.initPosRot.pos.y;
+                this->state = 0;
+                this->targetPosY = this->dyna.actor.initPosRot.pos.y;
             }
-        } else if (this->unk168 == 3) {
-            this->unk168 = 2;
-            this->unk16C = this->dyna.actor.initPosRot.pos.y + 140.0f;
+        } else if (this->state == 3) {
+            this->state = 2;
+            this->targetPosY = this->dyna.actor.initPosRot.pos.y + 140.0f;
         }
-        this->actionFunc = func_80870F00;
+        this->actionFunc = BgDdanJd_Move;
     }
 }
 
@@ -125,24 +124,24 @@ void func_80870D2C(BgDdanJd* this, GlobalContext* globalCtx) {
         sp34.z = this->dyna.actor.posRot.pos.z - 65.0f;
         func_80033480(globalCtx, &sp34, 5.0f, 1, 0x14, 0x3C, 1U);
     }
-    if (this->unk169 == 5) {
+    if (this->yVelocity == 5) {
         func_8002F974(&this->dyna.actor, NA_SE_EV_ELEVATOR_MOVE - SFX_FLAG);
     }
 }
 
-void func_80870F00(BgDdanJd* this, GlobalContext* globalCtx) {
-    if ((this->unk169 == 1) && (this->dyna.actor.params < 0x40) &&
+void BgDdanJd_Move(BgDdanJd* this, GlobalContext* globalCtx) {
+    if ((this->yVelocity == 1) && (this->dyna.actor.params < 0x40) &&
         Flags_GetSwitch(globalCtx, this->dyna.actor.params)) {
-        this->unk169 = 5;
-        this->unk168 = 1;
+        this->yVelocity = 5;
+        this->state = 1;
         this->dyna.actor.posRot.pos.y = this->dyna.actor.initPosRot.pos.y + 140.0f;
-        this->unk16A = 0;
-        this->actionFunc = func_80870B88;
+        this->timer = 0;
+        this->actionFunc = BgDdanJd_Idle;
         func_800800F8(globalCtx, 0xBF4, -0x63, &this->dyna.actor, 0);
     } else {
-        if (Math_ApproxF(&this->dyna.actor.posRot.pos.y, this->unk16C, this->unk169)) {
+        if (Math_ApproxF(&this->dyna.actor.posRot.pos.y, this->targetPosY, this->yVelocity)) {
             Audio_PlayActorSound2(&this->dyna.actor, NA_SE_EV_PILLAR_MOVE_STOP);
-            this->actionFunc = func_80870B88;
+            this->actionFunc = BgDdanJd_Idle;
         }
     }
     func_80870D2C(this, globalCtx);
--- src/overlays/z_bg_ddan_jd.h
+++ src/overlays/z_bg_ddan_jd.h
@@ -11,10 +11,10 @@ typedef void (*BgDdanJdActionFunc)(struct BgDdanJd*, GlobalContext*);
 typedef struct BgDdanJd {
     /* 0x000 */ DynaPolyActor dyna;
     /* 0x164 */ BgDdanJdActionFunc actionFunc;
-    /* 0x168 */ u8 unk168;
-    /* 0x169 */ u8 unk169;
-    /* 0x16A */ s16 unk16A;
-    /* 0x16C */ f32 unk16C;
+    /* 0x168 */ u8 state;
+    /* 0x169 */ u8 yVelocity;
+    /* 0x16A */ s16 timer;
+    /* 0x16C */ f32 targetPosY;
 } BgDdanJd; // size = 0x170
 
 extern const ActorInit Bg_Ddan_Jd_InitVars;
