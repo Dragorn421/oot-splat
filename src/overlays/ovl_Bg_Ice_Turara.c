57c435321edd80b2d6248ba5c0e70507d5198db1
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-16 20:53:42 +0100

    prepare for BgIceTurara decomp

---
+++ src/overlays/ovl_Bg_Ice_Turara.c
@@ -0,0 +1,50 @@
+/*
+ * File: z_bg_ice_turara.c
+ * Overlay: ovl_Bg_Ice_Turara
+ * Description: Icicles
+ */
+
+#include "z_bg_ice_turara.h"
+
+#define FLAGS 0x00000000
+
+#define THIS ((BgIceTurara*)thisx)
+
+void BgIceTurara_Init(Actor* thisx, GlobalContext* globalCtx);
+void BgIceTurara_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void BgIceTurara_Update(Actor* thisx, GlobalContext* globalCtx);
+void BgIceTurara_Draw(Actor* thisx, GlobalContext* globalCtx);
+
+/*
+const ActorInit Bg_Ice_Turara_InitVars = {
+    ACTOR_BG_ICE_TURARA,
+    ACTORTYPE_PROP,
+    FLAGS,
+    OBJECT_ICE_OBJECTS,
+    sizeof(BgIceTurara),
+    (ActorFunc)BgIceTurara_Init,
+    (ActorFunc)BgIceTurara_Destroy,
+    (ActorFunc)BgIceTurara_Update,
+    (ActorFunc)BgIceTurara_Draw,
+};
+*/
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/BgIceTurara_Init.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/BgIceTurara_Destroy.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/func_80892040.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/func_80892220.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/func_80892280.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/func_808922B8.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/func_80892424.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/func_80892574.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/BgIceTurara_Update.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ice_Turara/BgIceTurara_Draw.s")
---
+++ src/overlays/z_bg_ice_turara.h
@@ -0,0 +1,16 @@
+#ifndef _Z_BG_ICE_TURARA_H_
+#define _Z_BG_ICE_TURARA_H_
+
+#include "ultra64.h"
+#include "global.h"
+
+struct BgIceTurara;
+
+typedef struct BgIceTurara {
+    /* 0x0000 */ Actor actor;
+    /* 0x014C */ char unk_14C[0x6C];
+} BgIceTurara; // size = 0x01B8
+
+extern const ActorInit Bg_Ice_Turara_InitVars;
+
+#endif
