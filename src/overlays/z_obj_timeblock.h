263dd7e6eedc7247de03966b24d9390ca61a9d36
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-17 15:04:59 +0100

    ObjTimeblock decompiled

--- src/overlays/z_obj_timeblock.h
+++ src/overlays/z_obj_timeblock.h
@@ -7,13 +7,23 @@
 struct ObjTimeblock;
 
 typedef void (*ObjTimeblockActionFunc)(struct ObjTimeblock*, GlobalContext*);
+typedef s32 (*ObjTimeblockUnk168Func)(struct ObjTimeblock*, GlobalContext*);
 
 typedef struct ObjTimeblock {
-    /* 0x0000 */ DynaPolyActor dyna;
-    /* 0x0164 */ char unk_164[0x4];
-    /* 0x0168 */ ObjTimeblockActionFunc actionFunc;
-    /* 0x016C */ char unk_16C[0x10];
-} ObjTimeblock; // size = 0x017C
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x164 */ ObjTimeblockActionFunc unk164;
+    /* 0x168 */ ObjTimeblockUnk168Func unk168;
+    /* 0x16C */ s16 unk16C;
+    /* 0x16E */ s16 unk16E;
+    /* 0x170 */ s16 unk170;
+    /* 0x172 */ u16 unk172;
+    /* 0x174 */ u8 unk174;
+    /* 0x175 */ u8 unk175;
+    /* 0x176 */ u8 unk176;
+    /* 0x177 */ u8 unk177;
+    /* 0x178 */ u8 unk178;
+    /* 0x179 */ char pad179[3]; /* maybe part of unk178[4]? */
+} ObjTimeblock;                 // size = 0x017C
 
 extern const ActorInit Obj_Timeblock_InitVars;
 
