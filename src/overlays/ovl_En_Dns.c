df137cf7b0550fd0196d40e1429568946fc33921
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-17 08:07:42 +0100

    prepare for EnDns decomp

---
+++ src/overlays/ovl_En_Dns.c
@@ -0,0 +1,106 @@
+#include "z_en_dns.h"
+
+#define FLAGS 0x00000009
+
+#define THIS ((EnDns*)thisx)
+
+void EnDns_Init(Actor* thisx, GlobalContext* globalCtx);
+void EnDns_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void EnDns_Update(Actor* thisx, GlobalContext* globalCtx);
+void EnDns_Draw(Actor* thisx, GlobalContext* globalCtx);
+
+/*
+const ActorInit En_Dns_InitVars = {
+    ACTOR_EN_DNS,
+    ACTORTYPE_BG,
+    FLAGS,
+    OBJECT_SHOPNUTS,
+    sizeof(EnDns),
+    (ActorFunc)EnDns_Init,
+    (ActorFunc)EnDns_Destroy,
+    (ActorFunc)EnDns_Update,
+    (ActorFunc)EnDns_Draw,
+};
+*/
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F0560.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F057C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F0598.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F05B4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F05D0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F05EC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F0608.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F0624.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F0640.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F065C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/D_809F0678.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/EnDns_Init.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/EnDns_Destroy.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF51C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF5A4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF658.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF70C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF73C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF800.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF854.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF8F4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF9A4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EF9F8.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFA28.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFA58.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFA9C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFACC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFAFC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFB40.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFB84.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFBC8.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFC9C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFDD0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFEE8.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFF50.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809EFF98.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809F008C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809F0100.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/func_809F017C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/EnDns_Update.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Dns/EnDns_Draw.s")
