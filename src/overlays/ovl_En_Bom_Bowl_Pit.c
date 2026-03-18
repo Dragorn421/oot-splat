4824ee13618e50a4ca9a169328748110bf476f2c
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-18 18:22:08 +0100

    EnBomBowlPit cleanup

--- src/overlays/ovl_En_Bom_Bowl_Pit.c
+++ src/overlays/ovl_En_Bom_Bowl_Pit.c
@@ -77,25 +77,26 @@ void func_809C4E8C(EnBomBowlPit* this, GlobalContext* globalCtx) {
                 this->unk152 = Gameplay_CreateSubCamera(globalCtx);
                 Gameplay_ChangeCameraStatus(globalCtx, 0, 1);
                 Gameplay_ChangeCameraStatus(globalCtx, this->unk152, 7);
-                this->unk1A4 = this->unk1A8 = this->unk1AC = this->unk1C8 = this->unk1CC = this->unk1D0 = 0.1f;
+                this->unk1C8.x = this->unk1C8.y = this->unk1C8.z = 0.1f;
+                this->unk1A4.x = this->unk1A4.y = this->unk1A4.z = 0.1f;
                 this->unk180.x = this->unk168.x = globalCtx->view.lookAt.x;
                 this->unk180.y = this->unk168.y = globalCtx->view.lookAt.y;
                 this->unk180.z = this->unk168.z = globalCtx->view.lookAt.z;
                 this->unk18C.x = this->unk174.x = globalCtx->view.eye.x;
                 this->unk18C.y = this->unk174.y = globalCtx->view.eye.y;
                 this->unk18C.z = this->unk174.z = globalCtx->view.eye.z;
-                this->unk1BC = 20.0f;
-                this->unk1C0 = 100.0f;
-                this->unk1C4 = -800.0f;
-                this->unk198 = 20.0f;
-                this->unk19C = 50.0f;
-                this->unk1A0 = -485.0f;
-                this->unk1B0 = fabsf(this->unk18C.x - 20.0f) * 0.02f;
-                this->unk1B4 = fabsf(this->unk18C.y - 50.0f) * 0.02f;
-                this->unk1B8 = fabsf(this->unk18C.z - -485.0f) * 0.02f;
-                this->unk1D4 = fabsf(this->unk180.x - this->unk1BC) * 0.02f;
-                this->unk1D8 = fabsf(this->unk180.y - this->unk1C0) * 0.02f;
-                this->unk1DC = fabsf(this->unk180.z - this->unk1C4) * 0.02f;
+                this->unk1BC.x = 20.0f;
+                this->unk1BC.y = 100.0f;
+                this->unk1BC.z = -800.0f;
+                this->unk198.x = 20.0f;
+                this->unk198.y = 50.0f;
+                this->unk198.z = -485.0f;
+                this->unk1B0.x = fabsf(this->unk18C.x - 20.0f) * 0.02f;
+                this->unk1B0.y = fabsf(this->unk18C.y - 50.0f) * 0.02f;
+                this->unk1B0.z = fabsf(this->unk18C.z - -485.0f) * 0.02f;
+                this->unk1D4.x = fabsf(this->unk180.x - this->unk1BC.x) * 0.02f;
+                this->unk1D4.y = fabsf(this->unk180.y - this->unk1BC.y) * 0.02f;
+                this->unk1D4.z = fabsf(this->unk180.z - this->unk1BC.z) * 0.02f;
                 func_800C04D8(globalCtx, this->unk152, (Vec3f*)&this->unk180, (Vec3f*)&this->unk18C);
                 this->actor.textId = 0xF;
                 func_8010B680(globalCtx, this->actor.textId, NULL);
@@ -113,20 +114,20 @@ void func_809C4E8C(EnBomBowlPit* this, GlobalContext* globalCtx) {
 
 void func_809C5184(EnBomBowlPit* this, GlobalContext* globalCtx) {
     if (this->unk152 != 0) {
-        Math_SmoothScaleMaxF(&this->unk180.x, this->unk1BC, this->unk1C8, this->unk1D4);
-        Math_SmoothScaleMaxF(&this->unk180.y, this->unk1C0, this->unk1CC, this->unk1D8);
-        Math_SmoothScaleMaxF(&this->unk180.z, this->unk1C4, this->unk1D0, this->unk1DC);
-        Math_SmoothScaleMaxF(&this->unk18C.x, this->unk198, this->unk1A4, this->unk1B0);
-        Math_SmoothScaleMaxF(&this->unk18C.y, this->unk19C, this->unk1A8, this->unk1B4);
-        Math_SmoothScaleMaxF(&this->unk18C.z, this->unk1A0, this->unk1AC, this->unk1B8);
+        Math_SmoothScaleMaxF(&this->unk180.x, this->unk1BC.x, this->unk1C8.x, this->unk1D4.x);
+        Math_SmoothScaleMaxF(&this->unk180.y, this->unk1BC.y, this->unk1C8.y, this->unk1D4.y);
+        Math_SmoothScaleMaxF(&this->unk180.z, this->unk1BC.z, this->unk1C8.z, this->unk1D4.z);
+        Math_SmoothScaleMaxF(&this->unk18C.x, this->unk198.x, this->unk1A4.x, this->unk1B0.x);
+        Math_SmoothScaleMaxF(&this->unk18C.y, this->unk198.y, this->unk1A4.y, this->unk1B0.y);
+        Math_SmoothScaleMaxF(&this->unk18C.z, this->unk198.z, this->unk1A4.z, this->unk1B0.z);
     }
-    func_800C04D8(globalCtx, this->unk152, (Vec3f*)&this->unk180, (Vec3f*)&this->unk18C);
+    func_800C04D8(globalCtx, this->unk152, &this->unk180, &this->unk18C);
     if ((this->unk154 == func_8010BDBC(&globalCtx->msgCtx)) && (func_80106BC8(globalCtx) != 0)) {
         func_80106CCC(globalCtx);
     }
-    if ((fabsf(this->unk18C.x - this->unk198) < 5.0f) && (fabsf(this->unk18C.y - this->unk19C) < 5.0f) &&
-        (fabsf(this->unk18C.z - this->unk1A0) < 5.0f) && (fabsf(this->unk180.x - this->unk1BC) < 5.0f) &&
-        (fabsf(this->unk180.y - this->unk1C0) < 5.0f) && (fabsf(this->unk180.z - this->unk1C4) < 5.0f)) {
+    if ((fabsf(this->unk18C.x - this->unk198.x) < 5.0f) && (fabsf(this->unk18C.y - this->unk198.y) < 5.0f) &&
+        (fabsf(this->unk18C.z - this->unk198.z) < 5.0f) && (fabsf(this->unk180.x - this->unk1BC.x) < 5.0f) &&
+        (fabsf(this->unk180.y - this->unk1BC.y) < 5.0f) && (fabsf(this->unk180.z - this->unk1BC.z) < 5.0f)) {
         func_80106CCC(globalCtx);
         this->unk158 = 0x1E;
         this->unk14C = func_809C5360;
@@ -146,7 +147,7 @@ void func_809C5360(EnBomBowlPit* this, GlobalContext* globalCtx) {
 
 void func_809C53F0(EnBomBowlPit* this, GlobalContext* globalCtx) {
     if (this->unk156 != 0) {
-        switch (this->unk15A) { /* irregular */
+        switch (this->unk15A) {
             case 0:
                 gSaveContext.itemGetInf[1] |= 2;
                 break;
@@ -162,20 +163,17 @@ void func_809C53F0(EnBomBowlPit* this, GlobalContext* globalCtx) {
 }
 
 void func_809C54A8(EnBomBowlPit* this, GlobalContext* globalCtx) {
-    Player* temp_v0;
+    Player* player = PLAYER;
 
-    temp_v0 = PLAYER;
     func_8002DF54(globalCtx, NULL, 7U);
     this->unk160 = D_809C56E0[this->unk15A];
-    if ((this->unk160 == 0x33) &&
-        (gUpgradeCapacities[1][(s32)(gSaveContext.inventory.upgrades & gUpgradeMasks[1]) >> gUpgradeShifts[1]] ==
-         0x1E)) {
+    if ((this->unk160 == 0x33) && (CUR_CAPACITY(1) == 30)) {
         this->unk160 = 0x34;
     }
-    temp_v0->stateFlags1 &= 0xDFFFFFFF;
+    player->stateFlags1 &= ~0x20000000;
     this->actor.parent = NULL;
     func_8002F434(&this->actor, globalCtx, this->unk160, 2000.0f, 1000.0f);
-    temp_v0->stateFlags1 |= 0x20000000;
+    player->stateFlags1 |= 0x20000000;
     this->unk14C = func_809C55B0;
 }
 
@@ -201,36 +199,10 @@ void func_809C5608(EnBomBowlPit* this, GlobalContext* globalCtx) {
 }
 
 void EnBomBowlPit_Update(Actor* thisx, GlobalContext* globalCtx) {
-    s16 temp_v0;
     EnBomBowlPit* this = (EnBomBowlPit*)thisx;
 
     this->unk14C(this, globalCtx);
-    temp_v0 = this->unk158;
-    if (temp_v0 != 0) {
-        this->unk158 = temp_v0 - 1;
+    if (this->unk158 != 0) {
+        this->unk158--;
     }
 }
-
-/*
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/EnBomBowlPit_Init.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/EnBomBowlPit_Destroy.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/func_809C4E60.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/func_809C4E8C.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/func_809C5184.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/func_809C5360.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/func_809C53F0.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/func_809C54A8.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/func_809C55B0.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/func_809C5608.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Pit/EnBomBowlPit_Update.s")
-*/
--- src/overlays/z_en_bom_bowl_pit.h
+++ src/overlays/z_en_bom_bowl_pit.h
@@ -18,32 +18,18 @@ typedef struct EnBomBowlPit {
     /* 0x0158 */ s16 unk158;
     /* 0x015A */ s16 unk15A;
     /* 0x015C */ s16 unk15C;
-    /* 0x015E */ char pad15E[2];
     /* 0x0160 */ s32 unk160;
     /* 0x0164 */ u8 unk164;
-    /* 0x0165 */ char pad165[3]; /* maybe part of unk164[4]? */
     /* 0x0168 */ Vec3f unk168;
     /* 0x0174 */ Vec3f unk174;
     /* 0x0180 */ Vec3f unk180;
     /* 0x018C */ Vec3f unk18C;
-    /* 0x0198 */ f32 unk198;
-    /* 0x019C */ f32 unk19C;
-    /* 0x01A0 */ f32 unk1A0;
-    /* 0x01A4 */ f32 unk1A4;
-    /* 0x01A8 */ f32 unk1A8;
-    /* 0x01AC */ f32 unk1AC;
-    /* 0x01B0 */ f32 unk1B0;
-    /* 0x01B4 */ f32 unk1B4;
-    /* 0x01B8 */ f32 unk1B8;
-    /* 0x01BC */ f32 unk1BC;
-    /* 0x01C0 */ f32 unk1C0;
-    /* 0x01C4 */ f32 unk1C4;
-    /* 0x01C8 */ f32 unk1C8;
-    /* 0x01CC */ f32 unk1CC;
-    /* 0x01D0 */ f32 unk1D0;
-    /* 0x01D4 */ f32 unk1D4;
-    /* 0x01D8 */ f32 unk1D8;
-    /* 0x01DC */ f32 unk1DC;
+    /* 0x0198 */ Vec3f unk198;
+    /* 0x01A4 */ Vec3f unk1A4;
+    /* 0x01B0 */ Vec3f unk1B0;
+    /* 0x01BC */ Vec3f unk1BC;
+    /* 0x01C8 */ Vec3f unk1C8;
+    /* 0x01D4 */ Vec3f unk1D4;
     /* 0x01E0 */ Actor* unk1E0;
     /* 0x01E4 */ char pad1E4[0x3520];
 } EnBomBowlPit; // size = 0x3704
