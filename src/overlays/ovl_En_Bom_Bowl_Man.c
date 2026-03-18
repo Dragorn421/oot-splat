8f2fc9c2380ec3995ddee46ead56192426371497
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-18 15:58:43 +0100

    EnBomBowlMan data

--- src/overlays/ovl_En_Bom_Bowl_Man.c
+++ src/overlays/ovl_En_Bom_Bowl_Man.c
@@ -9,7 +9,11 @@ void EnBomBowlMan_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnBomBowlMan_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnBomBowlMan_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+extern AnimationHeader D_6000080;
+extern AnimationHeader D_6000710;
+extern FlexSkeletonHeader D_6006EB0;
+extern AnimationHeader D_60072AC;
+
 const ActorInit En_Bom_Bowl_Man_InitVars = {
     ACTOR_EN_BOM_BOWL_MAN,
     ACTORTYPE_NPC,
@@ -21,7 +25,22 @@ const ActorInit En_Bom_Bowl_Man_InitVars = {
     (ActorFunc)EnBomBowlMan_Update,
     (ActorFunc)EnBomBowlMan_Draw,
 };
-*/
+
+typedef struct struct_809C4A10 {
+    f32 unk0;
+    f32 unk4;
+    s32 unk8; // unused?
+} struct_809C4A10;
+struct_809C4A10 D_809C4A10[2] = { { 16.0f, 46.0f, 0 }, { 36.0f, 56.0f, 0 } };
+f32 D_809C4A28[6] = { 60.0f, -60.0f, -430.0f, 0.0f, -120.0f, -620.0f }; // unused?
+f32 D_809C4A40[2] = { 0.01f, 0.03f };
+Vec3f D_809C4A48 = { 0.0f, 0.1f, 0.0f };
+Vec3f D_809C4A54 = { 0.0f, 0.0f, 0.0f };
+Vec3f D_809C4A60[5] = {
+    { 0.0f, 22.0f, 0.0f }, { 0.0f, 22.0f, 0.0f }, { 0.0f, 8.0f, 0.0f }, { 0.0f, 9.0f, 0.0f }, { 0.0f, -2.0f, 0.0f },
+};
+static s16 D_809C4A9C[6] = { 0x4268, 0x4268, -0x3E8, 0, 0x4268, 0 };
+static s32 D_809C4AA8[6] = { 0x06004110, 0x06004910, 0x06005110, 0, 0, 0 };
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bom_Bowl_Man/EnBomBowlMan_Init.s")
 
