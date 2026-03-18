0560e8c87b4ca47181a8f69a3983c27e694e4684
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-18 21:47:18 +0100

    EnChanger data and analysis

--- src/overlays/ovl_En_Changer.c
+++ src/overlays/ovl_En_Changer.c
@@ -8,7 +8,10 @@ void EnChanger_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnChanger_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnChanger_Update(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+void func_809D2CCC(EnChanger* this, GlobalContext* globalCtx);
+void func_809D2D70(EnChanger* this, GlobalContext* globalCtx);
+void func_809D2F74(EnChanger* this, GlobalContext* globalCtx);
+
 const ActorInit En_Changer_InitVars = {
     ACTOR_EN_CHANGER,
     ACTORTYPE_PROP,
@@ -20,7 +23,18 @@ const ActorInit En_Changer_InitVars = {
     (ActorFunc)EnChanger_Update,
     NULL,
 };
-*/
+static Vec3f D_809D30A0[6] = {
+    { 0.0f, 0.0f, 0.0f },         { -100.0f, 20.0f, -245.0f },  { -100.0f, 20.0f, -685.0f },
+    { -100.0f, 20.0f, -1125.0f }, { -100.0f, 20.0f, -1565.0f }, { -100.0f, 20.0f, -2005.0f },
+};
+static Vec3f D_809D30E8[6] = {
+    { 0.0f, 0.0f, 0.0f },        { 140.0f, 20.0f, -245.0f },  { 140.0f, 20.0f, -685.0f },
+    { 140.0f, 20.0f, -1125.0f }, { 140.0f, 20.0f, -1565.0f }, { 140.0f, 20.0f, -2005.0f },
+};
+static s32 D_809D3130[6] = { 0, 0x72, 0x72, 0x73, 0x73, 0x74 };
+static s32 D_809D3148[6] = { 0, 8, 8, 9, 9, 0xA };
+static s32 D_809D3160[5] = { 0, 2, 4, 6, 8 };
+static s32 D_809D3174 = 0xA;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Changer/EnChanger_Destroy.s")
 
--- src/overlays/z_en_changer.h
+++ src/overlays/z_en_changer.h
@@ -6,9 +6,29 @@
 
 struct EnChanger;
 
+// EnBox
+typedef struct ActorA {
+    /* 0x000 */ Actor actor;
+    /* 0x14C */ char pad14C[0xA8];
+    /* 0x1F4 */ s16 unk1F4;
+} ActorA;
+
+typedef void (*EnChangerActionFunc)(struct EnChanger*, GlobalContext*);
+
 typedef struct EnChanger {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x20];
+    /* 0x000 */ Actor actor;
+    /* 0x14C */ EnChangerActionFunc unk14C;
+    /* 0x150 */ ActorA* unk150;
+    /* 0x154 */ ActorA* unk154;
+    /* 0x158 */ ActorA* unk158;
+    /* 0x15C */ s16 unk15C;
+    /* 0x15E */ s16 unk15E;
+    /* 0x160 */ s16 unk160;
+    /* 0x162 */ s16 unk162;
+    /* 0x164 */ char pad164[2];
+    /* 0x166 */ s16 unk166;
+    /* 0x168 */ s16 unk168;
+    /* 0x16A */ char pad16A[2];
 } EnChanger; // size = 0x016C
 
 extern const ActorInit En_Changer_InitVars;
