53663a6f903c3e34cc6192fb7a5b4cae9c674029
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-20 08:25:49 +0100

    prepare for BgJyaHaheniron decomp

---
+++ src/overlays/ovl_Bg_Jya_Haheniron.c
@@ -0,0 +1,56 @@
+/*
+ * File: z_bg_jya_haheniron
+ * Overlay: ovl_Bg_Jya_Haheniron
+ * Description: Chunks of Iron Knucle Chair and Pillar
+ */
+
+#include "z_bg_jya_haheniron.h"
+
+#define FLAGS 0x00000010
+
+#define THIS ((BgJyaHaheniron*)thisx)
+
+void BgJyaHaheniron_Init(Actor* thisx, GlobalContext* globalCtx);
+void BgJyaHaheniron_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void BgJyaHaheniron_Update(Actor* thisx, GlobalContext* globalCtx);
+void BgJyaHaheniron_Draw(Actor* thisx, GlobalContext* globalCtx);
+
+extern UNK_TYPE D_06000880;
+
+/*
+const ActorInit Bg_Jya_Haheniron_InitVars = {
+    ACTOR_BG_JYA_HAHENIRON,
+    ACTORTYPE_PROP,
+    FLAGS,
+    OBJECT_JYA_IRON,
+    sizeof(BgJyaHaheniron),
+    (ActorFunc)BgJyaHaheniron_Init,
+    (ActorFunc)BgJyaHaheniron_Destroy,
+    (ActorFunc)BgJyaHaheniron_Update,
+    (ActorFunc)BgJyaHaheniron_Draw,
+};
+*/
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_808980C0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_80898114.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/BgJyaHaheniron_Init.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/BgJyaHaheniron_Destroy.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_8089843C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_8089844C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_80898588.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_80898598.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_8089861C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_8089862C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/BgJyaHaheniron_Update.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/BgJyaHaheniron_Draw.s")
