5d706878408d3ffd6d07b73c907a786b9a1e48e1
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-20 08:41:36 +0100

    BgJyaHaheniron data and analysis

--- src/overlays/ovl_Bg_Jya_Haheniron.c
+++ src/overlays/ovl_Bg_Jya_Haheniron.c
@@ -15,9 +15,18 @@ void BgJyaHaheniron_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgJyaHaheniron_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgJyaHaheniron_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-extern UNK_TYPE D_06000880;
+void func_808980C0(BgJyaHaheniron* this, GlobalContext* globalCtx);
+void func_80898114(GlobalContext* globalCtx, Vec3f* arg1, Vec3f* arg2);
+void func_8089843C(BgJyaHaheniron* this);
+void func_8089844C(BgJyaHaheniron* this, GlobalContext* globalCtx);
+void func_80898588(BgJyaHaheniron* this);
+void func_80898598(BgJyaHaheniron* this, GlobalContext* globalCtx);
+void func_8089861C(BgJyaHaheniron* this);
+void func_8089862C(BgJyaHaheniron* this, GlobalContext* globalCtx);
 
-/*
+extern Gfx D_6000600[];
+extern Gfx D_6000880[];
+
 const ActorInit Bg_Jya_Haheniron_InitVars = {
     ACTOR_BG_JYA_HAHENIRON,
     ACTORTYPE_PROP,
@@ -29,7 +38,23 @@ const ActorInit Bg_Jya_Haheniron_InitVars = {
     (ActorFunc)BgJyaHaheniron_Update,
     (ActorFunc)BgJyaHaheniron_Draw,
 };
-*/
+static ColliderSpheresElementSrc D_80898740[1] = {
+    {
+        { 0, { 0xFFCFFFFF, 0, 4 }, { 0, 0, 0 }, 1, 0, 0 },
+        { 0, { { 0, 0, 0 }, 0xA }, 0x64 },
+    },
+};
+static ColliderSpheresSrc D_80898764 = { { 0xA, 0x11, 1, 0, 0, 0 }, 1, D_80898740 };
+static s16 D_80898774[5] = { 5, 8, 0xB, 0xE, 0x11 };
+static InitChainEntry D_80898780[5] = {
+    ICHAIN_F32_DIV1000(gravity, -2000, ICHAIN_CONTINUE),  ICHAIN_F32_DIV1000(minVelocityY, -15000, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneForward, 1000, ICHAIN_CONTINUE), ICHAIN_F32(uncullZoneScale, 500, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneDownward, 1000, ICHAIN_STOP),
+};
+static f32 D_80898794[3] = { 0.13f, 0.1f, 0.1f };
+static Vec3f D_808987A0 = { 0.0f, 14.0f, 0.0f };
+static Vec3f D_808987AC = { 0.0f, 8.0f, 0.0f };
+static Gfx* D_808987B8[6] = { D_6000880, (Gfx*)0x06000AE0, D_6000600, NULL, NULL, NULL };
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Haheniron/func_808980C0.s")
 
--- src/overlays/z_bg_jya_haheniron.h
+++ src/overlays/z_bg_jya_haheniron.h
@@ -6,9 +6,15 @@
 
 struct BgJyaHaheniron;
 
+typedef void (*BgJyaHahenironActionFunc)(struct BgJyaHaheniron*, GlobalContext*);
+
 typedef struct BgJyaHaheniron {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x68];
+    /* 0x000 */ Actor actor;
+    /* 0x14C */ BgJyaHahenironActionFunc unk14C;
+    /* 0x150 */ ColliderSpheres unk150;
+    /* 0x170 */ ColliderSpheresElement unk170[1];
+    /* 0x1B0 */ s16 unk1B0;
+    /* 0x1B2 */ char pad1B2[2];
 } BgJyaHaheniron; // size = 0x01B4
 
 extern const ActorInit Bg_Jya_Haheniron_InitVars;
