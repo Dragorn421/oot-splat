7feb4eb4d8a8a703207245fe0a519a5ed2c2e8f9
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-21 16:25:53 +0100

    BgJyaBigmirror cleanup

--- src/overlays/ovl_Bg_Jya_Bigmirror.c
+++ src/overlays/ovl_Bg_Jya_Bigmirror.c
@@ -58,13 +58,9 @@ void func_808936E0(BgJyaBigmirror* this, GlobalContext* globalCtx) {
     this->unk15C &= ~0x38;
     if (globalCtx->roomCtx.curRoom.num == 5) {
         this->unk15C |= 8;
-        return;
-    }
-    if (globalCtx->roomCtx.curRoom.num == 0x19) {
+    } else if (globalCtx->roomCtx.curRoom.num == 0x19) {
         this->unk15C |= 0x10;
-        return;
-    }
-    if (globalCtx->roomCtx.curRoom.num == 0x1A) {
+    } else if (globalCtx->roomCtx.curRoom.num == 0x1A) {
         this->unk15C |= 0x20;
     }
 }
@@ -87,39 +83,39 @@ void func_80893750(BgJyaBigmirror* this2, GlobalContext* globalCtx) {
                     this->unk15C &= ~D_80893F1C[i];
                 }
                 if (new_var->unk0->update == NULL) {
-                    osSyncPrintf("Error : コブラ削除された (%s %d)\n", "../z_bg_jya_bigmirror.c", 0xCB);
+                    osSyncPrintf("Error : コブラ削除された (%s %d)\n", "../z_bg_jya_bigmirror.c", 203);
                 }
             } else {
-                new_var->unk0 = Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, 0xFC,
+                new_var->unk0 = Actor_SpawnAsChild(&globalCtx->actorCtx, &this->actor, globalCtx, ACTOR_BG_JYA_COBRA,
                                                    new_var2->unk0.x, new_var2->unk0.y, new_var2->unk0.z, 0,
-                                                   (s16)(s32)new_var->unk4, 0, (s16)(s32)new_var2->unkC);
+                                                   new_var->unk4, 0, new_var2->unkC);
                 this->actor.child = NULL;
                 if (new_var->unk0 == NULL) {
-                    osSyncPrintf("Error : コブラ発生失敗 (%s %d)\n", "../z_bg_jya_bigmirror.c", 0xDD);
+                    osSyncPrintf("Error : コブラ発生失敗 (%s %d)\n", "../z_bg_jya_bigmirror.c", 221);
                 }
             }
         }
-        return;
-    }
-    for (i = 0; i < 2; i++) {
-        new_var = &this->unk14C[i];
-        if (new_var->unk0 != NULL) {
-            if (new_var->unk0->child != NULL) {
-                Actor_Kill(new_var->unk0->child);
-                new_var->unk0->child = NULL;
+    } else {
+        for (i = 0; i < 2; i++) {
+            new_var = &this->unk14C[i];
+            if (new_var->unk0 != NULL) {
+                if (new_var->unk0->child != NULL) {
+                    Actor_Kill(new_var->unk0->child);
+                    new_var->unk0->child = NULL;
+                }
+                Actor_Kill(new_var->unk0);
+                new_var->unk0 = NULL;
             }
-            Actor_Kill(new_var->unk0);
-            new_var->unk0 = NULL;
         }
     }
 }
 
 void func_8089394C(BgJyaBigmirror* this, GlobalContext* globalCtx) {
-    if (Flags_GetSwitch(globalCtx, 0x29) != 0) {
+    if (Flags_GetSwitch(globalCtx, 0x29)) {
         this->unk15C |= 4;
-        return;
+    } else {
+        this->unk15C &= ~4;
     }
-    this->unk15C &= 0xFFFB;
 }
 
 void func_8089399C(BgJyaBigmirror* this2, GlobalContext* globalCtx) {
@@ -129,7 +125,7 @@ void func_8089399C(BgJyaBigmirror* this2, GlobalContext* globalCtx) {
     BgJyaBigmirror* this = this2;
     s32 temp_v0;
 
-    temp_v0 = Object_GetIndex(&globalCtx->objectCtx, 0xAB);
+    temp_v0 = Object_GetIndex(&globalCtx->objectCtx, OBJECT_MIR_RAY);
     if ((temp_v0 < 0) || ((temp_v0 != this->unk16C))) {
         this->unk160[2] = NULL;
         this->unk160[1] = NULL;
@@ -150,9 +146,9 @@ void func_8089399C(BgJyaBigmirror* this2, GlobalContext* globalCtx) {
                 if ((this->unk160[var_s1] == NULL) && (Object_IsLoaded(&globalCtx->objectCtx, temp_v0) != 0)) {
                     this->unk160[var_s1] =
                         Actor_Spawn(&globalCtx->actorCtx, globalCtx, 0xB7, D_80893F28[var_s1].x, D_80893F28[var_s1].y,
-                                    D_80893F28[var_s1].z, 0, 0, 0, (s16)(s32)D_80893F20[var_s1]);
+                                    D_80893F28[var_s1].z, 0, 0, 0, D_80893F20[var_s1]);
                     if (this->unk160[var_s1] == NULL) {
-                        osSyncPrintf("Error : Mir Ray 発生失敗 (%s %d)\n", "../z_bg_jya_bigmirror.c", 0x136);
+                        osSyncPrintf("Error : Mir Ray 発生失敗 (%s %d)\n", "../z_bg_jya_bigmirror.c", 310);
                     }
                 }
             } else {
@@ -204,60 +200,37 @@ void func_80893C68(BgJyaBigmirror* this, GlobalContext* globalCtx) {
     s32 pad;
     Actor* temp_v0;
 
-    OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_jya_bigmirror.c", 0x1B3);
+    OPEN_DISPS(globalCtx->state.gfxCtx, "../z_bg_jya_bigmirror.c", 435);
     func_80093D84(globalCtx->state.gfxCtx);
-    temp_v0 = Actor_Find(&globalCtx->actorCtx, 0x157, 1);
+    temp_v0 = Actor_Find(&globalCtx->actorCtx, ACTOR_BG_JYA_LIFT, ACTORTYPE_BG);
     if (temp_v0 != NULL) {
         this->unk170 = temp_v0->posRot.pos.y;
     }
     func_800D1694(this->actor.posRot.pos.x, this->actor.posRot.pos.y + 40.0f, this->actor.posRot.pos.z,
                   &this->actor.shape.rot);
-    Matrix_Scale(0.1f, (this->unk170 * -0.00078125f) + 1.3901563f, 0.1f, 1U);
-    gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_jya_bigmirror.c", 0x1C9),
+    Matrix_Scale(0.1f, (this->unk170 * -0.00078125f) + 1.3901563f, 0.1f, MTXMODE_APPLY);
+    gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_jya_bigmirror.c", 457),
               G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     if (1) {}
     gSPDisplayList(POLY_XLU_DISP++, D_600BC70);
     if (temp_v0 != NULL) {
         func_800D1694(temp_v0->posRot.pos.x, temp_v0->posRot.pos.y, temp_v0->posRot.pos.z, &D_80893F4C);
-        Matrix_Scale(0.1f, 0.1f, 0.1f, 1U);
-        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_jya_bigmirror.c", 0x1D3),
+        Matrix_Scale(0.1f, 0.1f, 0.1f, MTXMODE_APPLY);
+        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx, "../z_bg_jya_bigmirror.c", 467),
                   G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         gSPDisplayList(POLY_XLU_DISP++, D_600BD80);
     }
-    CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_bg_jya_bigmirror.c", 0x1DC);
+    CLOSE_DISPS(globalCtx->state.gfxCtx, "../z_bg_jya_bigmirror.c", 476);
 }
 
 void BgJyaBigmirror_Draw(Actor* thisx, GlobalContext* globalCtx) {
-    u8 var_v0;
     BgJyaBigmirror* this = (BgJyaBigmirror*)thisx;
 
-    var_v0 = this->unk15C;
-    if (var_v0 & 0x10) {
+    if (this->unk15C & 0x10) {
         Gfx_DrawDListOpa(globalCtx, D_600E1B0);
         Gfx_DrawDListXlu(globalCtx, D_600E2D0);
-        var_v0 = this->unk15C;
     }
-    if ((var_v0 & 0x38) && (var_v0 & 2) && (var_v0 & 1)) {
+    if ((this->unk15C & 0x38) && (this->unk15C & 2) && (this->unk15C & 1)) {
         func_80893C68(this, globalCtx);
     }
 }
-
-/*
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/func_808936E0.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/func_80893750.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/func_8089394C.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/func_8089399C.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/BgJyaBigmirror_Init.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/BgJyaBigmirror_Destroy.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/BgJyaBigmirror_Update.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/func_80893C68.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Jya_Bigmirror/BgJyaBigmirror_Draw.s")
-*/
