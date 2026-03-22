684c904b69cd4b6501bfdea574ef9cfc19e85391
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-22 04:02:18 +0100

    add tools/first_diffs.py

---
+++ tools/first_diffs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+import mapfile_parser
+import numpy as np
+
+import sym_info
+
+
+baserom_p = Path("baserom.z64")
+built_rom_p = Path("build/rom.z64")
+
+built_map_path = Path("build/rom.map")
+built_elf_path = Path("build/rom.elf")
+
+baserom_bytes = np.frombuffer(baserom_p.read_bytes(), dtype=np.byte)
+built_rom_p = np.frombuffer(built_rom_p.read_bytes(), dtype=np.byte)
+
+n = min(len(baserom_bytes), len(built_rom_p))
+
+(diffs_inds,) = np.nonzero(baserom_bytes[:n] != built_rom_p[:n])
+
+map_file = mapfile_parser.mapfile.MapFile()
+map_file.readMapFile(built_map_path)
+
+local_symbols = sym_info.read_local_symbols_from_mdebug(built_elf_path)
+sym_info.merge_local_symbols(map_file, local_symbols)
+
+IGN_IMMEDIATES = True
+
+for off in diffs_inds:
+    if off % 4 >= 2 and IGN_IMMEDIATES:
+        # ignore potential immediates
+        continue
+    info, sections = map_file.findSymbolByVrom(off)
+    try:
+        print(f"0x{off:X}", info.getAsStrPlusOffset() if info is not None else "?")
+    except BrokenPipeError:
+        break
