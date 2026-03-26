db706c25d2d9759d4d313aa9bd0df56bb71cd1b0
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-26 13:32:23 +0100

    prepare for EnWood02 decomp

---
+++ src/overlays/ovl_En_Wood02.c
@@ -0,0 +1,44 @@
+/*
+ * File: z_en_wood02.c
+ * Overlay: ovl_En_Wood02
+ * Description: Trees & Bushes
+ */
+
+#include "z_en_wood02.h"
+
+#define FLAGS 0x00000000
+
+#define THIS ((EnWood02*)thisx)
+
+void EnWood02_Init(Actor* thisx, GlobalContext* globalCtx);
+void EnWood02_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void EnWood02_Update(Actor* thisx, GlobalContext* globalCtx);
+void EnWood02_Draw(Actor* thisx, GlobalContext* globalCtx);
+
+extern UNK_TYPE D_06000700;
+
+/*
+const ActorInit En_Wood02_InitVars = {
+    ACTOR_EN_WOOD02,
+    ACTORTYPE_PROP,
+    FLAGS,
+    OBJECT_WOOD02,
+    sizeof(EnWood02),
+    (ActorFunc)EnWood02_Init,
+    (ActorFunc)EnWood02_Destroy,
+    (ActorFunc)EnWood02_Update,
+    (ActorFunc)EnWood02_Draw,
+};
+*/
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Wood02/func_80B3AF70.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Wood02/func_80B3B094.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Wood02/EnWood02_Init.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Wood02/EnWood02_Destroy.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Wood02/EnWood02_Update.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Wood02/EnWood02_Draw.s")
