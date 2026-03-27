d7a7d7dce9491216c6c91a27778ecd59aa47ca11
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-27 13:19:41 +0100

    DemoExt cleanup

--- src/overlays/ovl_Demo_Ext.c
+++ src/overlays/ovl_Demo_Ext.c
@@ -26,12 +26,14 @@ void func_80977950(DemoExt* arg0, GlobalContext* arg1);
 
 extern Gfx D_600FAA0[];
 
-void (*D_80977C70[3])(DemoExt*, GlobalContext*) = {
+typedef void (*DemoExtUpdateFunc)(DemoExt*, GlobalContext*);
+DemoExtUpdateFunc D_80977C70[] = {
     func_80977854,
     func_80977874,
     func_809778AC,
 };
-void (*D_80977C7C[2])(DemoExt*, GlobalContext*) = {
+typedef void (*DemoExtDrawFunc)(DemoExt*, GlobalContext*);
+DemoExtDrawFunc D_80977C7C[] = {
     func_80977944,
     func_80977950,
 };
@@ -48,7 +50,6 @@ const ActorInit Demo_Ext_InitVars = {
 };
 
 void DemoExt_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    DemoExt* this = (DemoExt*)thisx;
 }
 
 void DemoExt_Init(Actor* thisx, GlobalContext* globalCtx) {
@@ -59,188 +60,186 @@ void DemoExt_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->unk158[3] = 0x1E;
     this->unk170 = gGameInfo->data[0xA3C] + 0xFF;
     this->unk174 = gGameInfo->data[0xA40] + 0xFF;
-    this->unk178.x = (f32)gGameInfo->data[0xA33] + 400.0f;
-    this->unk178.y = (f32)gGameInfo->data[0xA34] + 100.0f;
-    this->unk178.z = (f32)gGameInfo->data[0xA35] + 400.0f;
+    this->unk178.x = gGameInfo->data[0xA33] + 400.0f;
+    this->unk178.y = gGameInfo->data[0xA34] + 100.0f;
+    this->unk178.z = gGameInfo->data[0xA35] + 400.0f;
 }
 
-void func_80977450(DemoExt* arg0) {
-    if (arg0->unk16C <= (((f32)gGameInfo->data[0xA43] + 40.0f) - 15.0f)) {
-        Audio_PlaySoundGeneral(0x2040U, &arg0->actor.projectedPos, 4U, &D_801333E0, &D_801333E0, &D_801333E8);
+void func_80977450(DemoExt* this) {
+    if (this->unk16C <= (gGameInfo->data[0xA43] + 40.0f - 15.0f)) {
+        Audio_PlaySoundGeneral(NA_SE_EV_FANTOM_WARP_L - SFX_FLAG, &this->actor.projectedPos, 4U, &D_801333E0,
+                               &D_801333E0, &D_801333E8);
     }
 }
 
-CsCmdActorAction* func_809774D8(GlobalContext* arg0, s32 arg1) {
-    if (arg0->csCtx.state != 0) {
-        return arg0->csCtx.npcActions[arg1];
+CsCmdActorAction* func_809774D8(GlobalContext* globalCtx, s32 arg1) {
+    if (globalCtx->csCtx.state != CS_STATE_IDLE) {
+        return globalCtx->csCtx.npcActions[arg1];
     }
     return NULL;
 }
 
-void func_809774FC(DemoExt* arg0) {
-    arg0->unk14C = 0;
-    arg0->unk150 = 0;
+void func_809774FC(DemoExt* this) {
+    this->unk14C = 0;
+    this->unk150 = 0;
 }
 
-void func_80977508(DemoExt* arg0, GlobalContext* arg1) {
+void func_80977508(DemoExt* this, GlobalContext* globalCtx) {
     CsCmdActorAction* temp_v0;
     s16 temp_v1;
 
-    temp_v0 = func_809774D8(arg1, 5);
+    temp_v0 = func_809774D8(globalCtx, 5);
     if (temp_v0 != NULL) {
-        arg0->actor.posRot.pos.x = (f32)temp_v0->startPos.x;
-        arg0->actor.posRot.pos.y = (f32)temp_v0->startPos.y;
-        arg0->actor.posRot.pos.z = (f32)temp_v0->startPos.z;
+        this->actor.posRot.pos.x = temp_v0->startPos.x;
+        this->actor.posRot.pos.y = temp_v0->startPos.y;
+        this->actor.posRot.pos.z = temp_v0->startPos.z;
         temp_v1 = temp_v0->rot.y;
-        arg0->actor.shape.rot.y = temp_v1;
-        arg0->actor.posRot.rot.y = temp_v1;
+        this->actor.shape.rot.y = temp_v1;
+        this->actor.posRot.rot.y = temp_v1;
     }
-    arg0->unk14C = 1;
-    arg0->unk150 = 1;
+    this->unk14C = 1;
+    this->unk150 = 1;
 }
 
-void func_80977590(DemoExt* arg0) {
-    arg0->unk14C = 2;
-    arg0->unk150 = 1;
+void func_80977590(DemoExt* this) {
+    this->unk14C = 2;
+    this->unk150 = 1;
 }
 
-void func_809775A4(DemoExt* arg0) {
-    arg0->unk16C += 1.0f;
-    if (((f32)gGameInfo->data[0xA43] + 40.0f) <= arg0->unk16C) {
-        Actor_Kill(&arg0->actor);
+void func_809775A4(DemoExt* this) {
+    this->unk16C += 1.0f;
+    if ((gGameInfo->data[0xA43] + 40.0f) <= this->unk16C) {
+        Actor_Kill(&this->actor);
     }
 }
 
-void func_80977610(DemoExt* arg0, GlobalContext* arg1) {
+void func_80977610(DemoExt* this, GlobalContext* globalCtx) {
     CsCmdActorAction* temp_v0;
     s32 temp_a2;
     s32 v;
 
-    temp_v0 = func_809774D8(arg1, 5);
+    temp_v0 = func_809774D8(globalCtx, 5);
     if (temp_v0 != NULL) {
         temp_a2 = temp_v0->action;
-        v = arg0->unk154;
+        v = this->unk154;
         if (temp_a2 != v) {
             switch (temp_a2) {
                 case 1:
-                    func_809774FC(arg0);
+                    func_809774FC(this);
                     break;
+
                 case 2:
-                    func_80977508(arg0, arg1);
+                    func_80977508(this, globalCtx);
                     break;
+
                 case 3:
-                    func_80977590(arg0);
+                    func_80977590(this);
                     break;
+
                 default:
                     osSyncPrintf("Demo_Ext_Check_DemoMode:そんな動作は無い!!!!!!!!\n");
                     break;
             }
-            arg0->unk154 = temp_a2;
+            this->unk154 = temp_a2;
         }
     }
 }
 
-void func_809776D0(DemoExt* arg0) {
-    s16* p1 = arg0->unk160;
-    s16* p2 = arg0->unk158;
+void func_809776D0(DemoExt* this) {
+    s16* p1 = this->unk160;
+    s16* p2 = this->unk158;
     s32 var_v0;
 
     for (var_v0 = 3; var_v0 != 0; var_v0--) {
         p1[var_v0] += p2[var_v0];
     }
-    arg0->unk168 += (s16)(gGameInfo->data[0xA42] + 0x3E8);
+    this->unk168 += (s16)(gGameInfo->data[0xA42] + 0x3E8);
 }
 
-void func_8097771C(DemoExt* arg0) {
+void func_8097771C(DemoExt* this) {
     Vec3f* temp_v0;
     f32 var_fv0;
 
-    temp_v0 = &arg0->unk178;
-    var_fv0 = (((f32)gGameInfo->data[0xA43] + 40.0f) - arg0->unk16C) / ((f32)gGameInfo->data[0xA43] + 40.0f);
+    temp_v0 = &this->unk178;
+    var_fv0 = ((gGameInfo->data[0xA43] + 40.0f) - this->unk16C) / (gGameInfo->data[0xA43] + 40.0f);
     if (var_fv0 < 0.0f) {
         var_fv0 = 0.0f;
     }
-    arg0->unk170 = (s32)((u32)(gGameInfo->data[0xA3C] + 0xFF) * var_fv0);
-    arg0->unk174 = (s32)((u32)(gGameInfo->data[0xA40] + 0xFF) * var_fv0);
-    temp_v0->x = ((f32)gGameInfo->data[0xA33] + 400.0f) * var_fv0;
-    temp_v0->y = (f32)(((f32)gGameInfo->data[0xA34] + 100.0f) * var_fv0);
-    temp_v0->z = (f32)(((f32)gGameInfo->data[0xA35] + 400.0f) * var_fv0);
+    this->unk170 = (s32)((u32)(gGameInfo->data[0xA3C] + 0xFF) * var_fv0);
+    this->unk174 = (s32)((u32)(gGameInfo->data[0xA40] + 0xFF) * var_fv0);
+    temp_v0->x = (gGameInfo->data[0xA33] + 400.0f) * var_fv0;
+    temp_v0->y = (gGameInfo->data[0xA34] + 100.0f) * var_fv0;
+    temp_v0->z = (gGameInfo->data[0xA35] + 400.0f) * var_fv0;
 }
 
-void func_80977854(DemoExt* arg0, GlobalContext* arg1) {
-    func_80977610(arg0, arg1);
+void func_80977854(DemoExt* this, GlobalContext* globalCtx) {
+    func_80977610(this, globalCtx);
 }
 
-void func_80977874(DemoExt* arg0, GlobalContext* arg1) {
-    func_80977450(arg0);
-    func_809776D0(arg0);
-    func_80977610(arg0, arg1);
+void func_80977874(DemoExt* this, GlobalContext* globalCtx) {
+    func_80977450(this);
+    func_809776D0(this);
+    func_80977610(this, globalCtx);
 }
 
-void func_809778AC(DemoExt* arg0, GlobalContext* arg1) {
-    func_80977450(arg0);
-    func_809776D0(arg0);
-    func_8097771C(arg0);
-    func_809775A4(arg0);
+void func_809778AC(DemoExt* this, GlobalContext* globalCtx) {
+    func_80977450(this);
+    func_809776D0(this);
+    func_8097771C(this);
+    func_809775A4(this);
 }
 
 void DemoExt_Update(Actor* thisx, GlobalContext* globalCtx) {
-    s32 temp_v0;
-    void (*temp_v1)(DemoExt*, GlobalContext*);
     DemoExt* this = (DemoExt*)thisx;
 
-    temp_v0 = this->unk14C;
-    if ((temp_v0 < 0) || (temp_v0 >= 3) || (temp_v1 = D_80977C70[temp_v0], (temp_v1 == NULL))) {
+    if ((this->unk14C < 0) || (this->unk14C >= ARRAY_COUNT(D_80977C70)) || (D_80977C70[this->unk14C] == NULL)) {
         osSyncPrintf("\x1b[31mメインモードがおかしい!!!!!!!!!!!!!!!!!!!!!!!!!\n\x1b[m");
         return;
     }
-    temp_v1(this, globalCtx);
+    D_80977C70[this->unk14C](this, globalCtx);
 }
 
-void func_80977944(DemoExt* arg0, GlobalContext* arg1) {
+void func_80977944(DemoExt* this, GlobalContext* globalCtx) {
 }
 
-void func_80977950(DemoExt* arg0, GlobalContext* arg1) {
+void func_80977950(DemoExt* this, GlobalContext* globalCtx) {
     GraphicsContext* temp_s0;
     Mtx* sp80;
     Vec3f* temp_v0;
     s16* temp_v0_2;
     s32 pad;
 
-    temp_s0 = arg1->state.gfxCtx;
-    temp_v0_2 = arg0->unk160;
-    temp_v0 = &arg0->unk178;
+    temp_s0 = globalCtx->state.gfxCtx;
+    temp_v0_2 = this->unk160;
+    temp_v0 = &this->unk178;
     sp80 = Graph_Alloc(temp_s0, 0x40U);
-    OPEN_DISPS(temp_s0, "../z_demo_ext.c", 0x1CC);
+    OPEN_DISPS(temp_s0, "../z_demo_ext.c", 460);
     Matrix_Push();
     Matrix_Scale(temp_v0->x, temp_v0->y, temp_v0->z, 1U);
-    Matrix_RotateRPY((s16)(gGameInfo->data[0xA30] + 0x4000), arg0->unk168, gGameInfo->data[0xA32], 1U);
+    Matrix_RotateRPY((s16)(gGameInfo->data[0xA30] + 0x4000), this->unk168, gGameInfo->data[0xA32], 1U);
     Matrix_Translate((f32)gGameInfo->data[0xA36], (f32)gGameInfo->data[0xA37], (f32)gGameInfo->data[0xA38], 1U);
-    Matrix_ToMtx(sp80, "../z_demo_ext.c", 0x1DC);
+    Matrix_ToMtx(sp80, "../z_demo_ext.c", 476);
     Matrix_Pull();
     func_80093D84(temp_s0);
     gDPSetPrimColor(POLY_XLU_DISP++, 0x00, gGameInfo->data[0xA41] + 0x80, gGameInfo->data[0xA39] + 0x8C,
-                    gGameInfo->data[0xA3A] + 0x50, gGameInfo->data[0xA3B] + 0x8C, arg0->unk170);
+                    gGameInfo->data[0xA3A] + 0x50, gGameInfo->data[0xA3B] + 0x8C, this->unk170);
     gDPSetEnvColor(POLY_XLU_DISP++, gGameInfo->data[0xA3D] + 0x5A, gGameInfo->data[0xA3E] + 0x32,
-                   gGameInfo->data[0xA3F] + 0x5F, arg0->unk174);
+                   gGameInfo->data[0xA3F] + 0x5F, this->unk174);
     gSPSegment(POLY_XLU_DISP++, 0x08,
                Gfx_TwoTexScroll(temp_s0, 0, (u32)temp_v0_2[0], (u32)temp_v0_2[1], 0x40, 0x40, 1, (u32)temp_v0_2[2],
                                 (u32)temp_v0_2[3], 0x40, 0x40));
     gSPMatrix(POLY_XLU_DISP++, sp80, G_MTX_PUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     gSPDisplayList(POLY_XLU_DISP++, D_600FAA0);
     gSPPopMatrix(POLY_XLU_DISP++, G_MTX_MODELVIEW);
-    CLOSE_DISPS(temp_s0, "../z_demo_ext.c", 0x200);
+    CLOSE_DISPS(temp_s0, "../z_demo_ext.c", 512);
 }
 
 void DemoExt_Draw(Actor* thisx, GlobalContext* globalCtx) {
-    s32 temp_v0;
-    void (*temp_v1)(DemoExt*, GlobalContext*);
     DemoExt* this = (DemoExt*)thisx;
 
-    temp_v0 = this->unk150;
-    if ((temp_v0 < 0) || (temp_v0 >= 2) || (temp_v1 = D_80977C7C[temp_v0], (temp_v1 == NULL))) {
+    if ((this->unk150 < 0) || (this->unk150 >= ARRAY_COUNT(D_80977C7C)) || (D_80977C7C[this->unk150] == NULL)) {
         osSyncPrintf("\x1b[31m描画モードがおかしい!!!!!!!!!!!!!!!!!!!!!!!!!\n\x1b[m");
         return;
     }
-    temp_v1(this, globalCtx);
+    D_80977C7C[this->unk150](this, globalCtx);
 }
