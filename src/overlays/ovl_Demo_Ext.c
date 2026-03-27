809b4ffe9ba64959c953561576d7cdd4bd91762c
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-27 08:34:09 +0100

    prepare for DemoExt decomp (from oot_cc0redo_take2 @ 53b65a483a07dfe99a9f022b33f91c09dcce34de)

---
+++ src/overlays/ovl_Demo_Ext.c
@@ -0,0 +1,62 @@
+#include "z_demo_ext.h"
+
+#define FLAGS 0x00000010
+
+#define THIS ((DemoExt*)thisx)
+
+void DemoExt_Init(Actor* thisx, GlobalContext* globalCtx);
+void DemoExt_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void DemoExt_Update(Actor* thisx, GlobalContext* globalCtx);
+void DemoExt_Draw(Actor* thisx, GlobalContext* globalCtx);
+
+extern UNK_TYPE D_0600FAA0;
+
+/*
+const ActorInit Demo_Ext_InitVars = {
+    ACTOR_DEMO_EXT,
+    ACTORTYPE_NPC,
+    FLAGS,
+    OBJECT_FHG,
+    sizeof(DemoExt),
+    (ActorFunc)DemoExt_Init,
+    (ActorFunc)DemoExt_Destroy,
+    (ActorFunc)DemoExt_Update,
+    (ActorFunc)DemoExt_Draw,
+};
+*/
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/DemoExt_Destroy.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/DemoExt_Init.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_80977450.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_809774D8.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_809774FC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_80977508.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_80977590.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_809775A4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_80977610.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_809776D0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_8097771C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_80977854.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_80977874.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_809778AC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/DemoExt_Update.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_80977944.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/func_80977950.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Demo_Ext/DemoExt_Draw.s")
---
+++ src/overlays/z_demo_ext.h
@@ -0,0 +1,16 @@
+#ifndef _Z_DEMO_EXT_H_
+#define _Z_DEMO_EXT_H_
+
+#include "ultra64.h"
+#include "global.h"
+
+struct DemoExt;
+
+typedef struct DemoExt {
+    /* 0x0000 */ Actor actor;
+    /* 0x014C */ char unk_14C[0x38];
+} DemoExt; // size = 0x0184
+
+extern const ActorInit Demo_Ext_InitVars;
+
+#endif
