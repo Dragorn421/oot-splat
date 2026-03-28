60053f9418722ce049d4a93b5cfce67905b37de4
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-28 18:07:36 +0100

    EnBili data

--- src/overlays/ovl_En_Bili.c
+++ src/overlays/ovl_En_Bili.c
@@ -12,9 +12,9 @@ void EnBili_Draw(Actor* thisx, GlobalContext* globalCtx);
 extern UNK_TYPE D_06000024;
 extern UNK_TYPE D_06000064;
 extern UNK_TYPE D_060000A4;
+extern UNK_TYPE D_6004408;
 extern UNK_TYPE D_06005848;
 
-/*
 const ActorInit En_Bili_InitVars = {
     ACTOR_EN_BILI,
     ACTORCAT_ENEMY,
@@ -46,7 +46,36 @@ static ColliderCylinderInit D_809C1640 = {
     },
     { 9, 28, -20, { 0, 0, 0 } },
 };
-*/
+
+static CollisionCheckInfoInit2 D_809C166C = { 1, 9, 0x1C, -0x14, 0x1E };
+static DamageTable D_809C1678 = {
+    {
+        0x10, 2,    0xE0, 2, 1, 2, 2, 2, 0xF1, 0xF2, 0xF4, 0x24, 0x34, 2, 2, 2,
+        2,    0x24, 0x34, 0, 0, 0, 1, 4, 2,    2,    8,    4,    0,    0, 4, 0,
+    },
+};
+static InitChainEntry D_809C1698[] = {
+    ICHAIN_S8(naviEnemyId, 23, ICHAIN_CONTINUE),
+    ICHAIN_F32(targetArrowOffset, 2000, ICHAIN_STOP),
+};
+static Color_RGBA8 D_809C16A0 = { 0xFF, 0xFF, 0xFF, 0xFF };
+static Color_RGBA8 D_809C16A4 = { 0xC8, 0xFF, 0xFF, 0xFF };
+static Vec3f D_809C16A8 = { 0.0f, 0.0f, 0.0f };
+static Vec3f D_809C16B4 = { 0.0f, 0.0f, 0.0f };
+static Vec3f D_809C16C0 = { 1.0f, 1.0f, 1.0f };
+static s32 D_809C16CC[9] = {
+    0x06000E08, 0x06001708, 0x06002008, 0x06002908, 0x06003208, 0x06003B08, (s32)&D_6004408, 0x06004D08, 0,
+};
+static Gfx D_809C16F0[] = {
+    gsDPSetCombineLERP(1, TEXEL0, SHADE, 0, TEXEL0, 0, PRIMITIVE, 0, COMBINED, 0, PRIMITIVE, 0, TEXEL1, 0,
+                       PRIM_LOD_FRAC, COMBINED),
+    gsSPEndDisplayList(),
+};
+static Gfx D_809C1700[] = {
+    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, TEXEL0, 0, PRIMITIVE, 0, COMBINED, 0, PRIMITIVE, 0, TEXEL1, 0,
+                       PRIM_LOD_FRAC, COMBINED),
+    gsSPEndDisplayList(),
+};
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bili/EnBili_Init.s")
 
