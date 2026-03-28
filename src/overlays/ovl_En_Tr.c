e9da6b12e2e43d31758a2579a03359bb454102fc
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-28 09:37:12 +0100

    EnTr data

--- src/overlays/ovl_En_Tr.c
+++ src/overlays/ovl_En_Tr.c
@@ -15,15 +15,15 @@ void EnTr_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnTr_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnTr_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-extern UNK_TYPE D_060013CC;
-extern UNK_TYPE D_06001CDC;
-extern UNK_TYPE D_060035CC;
-extern UNK_TYPE D_06003FC8;
-extern UNK_TYPE D_060049C8;
-extern UNK_TYPE D_0600C530;
-extern UNK_TYPE D_06011688;
+extern AnimationHeader D_6000BFC;
+extern AnimationHeader D_60013CC;
+extern UNK_TYPE D_6001CDC;
+extern AnimationHeader D_60035CC;
+extern UNK_TYPE D_6003FC8;
+extern AnimationHeader D_60049C8;
+extern UNK_TYPE D_600C530;
+extern UNK_TYPE D_6011688;
 
-/*
 const ActorInit En_Tr_InitVars = {
     ACTOR_EN_TR,
     ACTORCAT_NPC,
@@ -35,7 +35,24 @@ const ActorInit En_Tr_InitVars = {
     (ActorFunc)EnTr_Update,
     (ActorFunc)EnTr_Draw,
 };
-*/
+static UNK_PTR D_80B24360[2] = { &D_6003FC8, &D_6001CDC };
+static AnimationHeader* D_80B24368[2] = { (AnimationHeader*)0x06002BC4, &D_6000BFC };
+static AnimationHeader* D_80B24370[2] = { &D_60035CC, &D_60013CC };
+static AnimationHeader* D_80B24378[2] = { &D_60049C8, &D_60049C8 };
+static AnimationHeader* D_80B24380[2] = { (AnimationHeader*)0x06012E1C, (AnimationHeader*)0x06012E1C };
+static f32 D_80B24388[7] = { 0.0f, 20.0f, -30.0f, 20.0f, -20.0f, -20.0f, 30.0f };
+static f32 D_80B243A4[7] = { 0.0f, 30.0f, 0.0f, -30.0f, 30.0f, -30.0f, 0.0f };
+typedef struct _struct_D_80B243C0_0x8 {
+    /* 0x0 */ Color_RGBA8 unk0;
+    /* 0x4 */ Color_RGBA8 unk4;
+} _struct_D_80B243C0_0x8; // size = 0x8
+static _struct_D_80B243C0_0x8 D_80B243C0[2] = {
+    { { 0xFF, 0xC8, 0, 0xFF }, { 0xFF, 0, 0, 0xFF } },
+    { { 0xFF, 0xFF, 0xFF, 0xFF }, { 0, 0, 0xFF, 0xFF } },
+};
+static s32 D_80B243D0[3] = { 0x060086D8, 0x060094D8, 0x060098D8 };
+static Vec3f D_80B243DC = { 2300.0f, 0.0f, -600.0f };
+static Vec3f D_80B243E8 = { 0.0f, 0.0f, 0.0f };
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Tr/func_80B22CF0.s")
 
