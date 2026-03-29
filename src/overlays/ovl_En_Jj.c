4a3ba010053670c0d0942a572d6a2de69aa8477b
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-29 22:09:48 +0200

    prepare EnJj for decomp

---
+++ src/overlays/ovl_En_Jj.c
@@ -0,0 +1,82 @@
+/*
+ * File: z_en_jj.c
+ * Overlay: ovl_En_Jj
+ * Description: Lord Jabu-Jabu
+ */
+
+#include "z_en_jj.h"
+
+#define FLAGS 0x00000030
+
+#define THIS ((EnJj*)thisx)
+
+void EnJj_Init(Actor* thisx, GlobalContext* globalCtx);
+void EnJj_Destroy(Actor* thisx, GlobalContext* globalCtx);
+void EnJj_Update(Actor* thisx, GlobalContext* globalCtx);
+void EnJj_Draw(Actor* thisx, GlobalContext* globalCtx);
+
+extern UNK_TYPE D_06000A1C;
+extern UNK_TYPE D_06001830;
+extern UNK_TYPE D_06001F4C;
+extern UNK_TYPE D_0600B9A8;
+extern UNK_TYPE D_0600BA8C;
+
+/*
+const ActorInit En_Jj_InitVars = {
+    ACTOR_EN_JJ,
+    ACTORCAT_ITEMACTION,
+    FLAGS,
+    OBJECT_JJ,
+    sizeof(EnJj),
+    (ActorFunc)EnJj_Init,
+    (ActorFunc)EnJj_Destroy,
+    (ActorFunc)EnJj_Update,
+    (ActorFunc)EnJj_Draw,
+};
+
+static ColliderCylinderInit D_80A88CB4 = {
+    {
+        COLTYPE_NONE,
+        AT_NONE,
+        AC_ON | AC_TYPE_PLAYER,
+        OC1_ON | OC1_TYPE_ALL,
+        OC2_TYPE_1,
+        COLSHAPE_CYLINDER,
+    },
+    {
+        ELEMTYPE_UNK0,
+        { 0x00000000, 0x00, 0x00 },
+        { 0x00000004, 0x00, 0x00 },
+        TOUCH_NONE,
+        BUMP_ON,
+        OCELEM_ON,
+    },
+    { 170, 150, 0, { 0, 0, 0 } },
+};
+*/
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87800.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/EnJj_Init.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/EnJj_Destroy.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87B1C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87B9C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87BEC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87C30.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87CEC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87D94.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87EF0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/func_80A87F44.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/EnJj_Update.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Jj/EnJj_Draw.s")
---
+++ src/overlays/z_en_jj.h
@@ -0,0 +1,16 @@
+#ifndef _Z_EN_JJ_H_
+#define _Z_EN_JJ_H_
+
+#include "ultra64.h"
+#include "global.h"
+
+struct EnJj;
+
+typedef struct EnJj {
+    /* 0x0000 */ Actor actor;
+    /* 0x014C */ char unk_14C[0x1C8];
+} EnJj; // size = 0x0314
+
+extern const ActorInit En_Jj_InitVars;
+
+#endif
