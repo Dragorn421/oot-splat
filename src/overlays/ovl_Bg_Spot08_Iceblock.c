892e67cb73183f0c892b73af47072cad591a91ba
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-29 11:44:52 +0200

    BgSpot08Iceblock analysis

--- src/overlays/ovl_Bg_Spot08_Iceblock.c
+++ src/overlays/ovl_Bg_Spot08_Iceblock.c
@@ -9,10 +9,29 @@ void BgSpot08Iceblock_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgSpot08Iceblock_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgSpot08Iceblock_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-extern UNK_TYPE D_06000DE0;
-extern UNK_TYPE D_06002BD0;
-extern UNK_TYPE D_06002FD8;
-extern UNK_TYPE D_06001904;
+void func_808B0960(BgSpot08Iceblock* this, void (*arg1)(BgSpot08Iceblock*, GlobalContext*));
+void func_808B0968(BgSpot08Iceblock* this, GlobalContext* globalCtx, CollisionHeader* arg2, s32 arg3);
+void func_808B09F0(BgSpot08Iceblock* this);
+void func_808B0A84(BgSpot08Iceblock* this);
+void func_808B0AE0(BgSpot08Iceblock* this);
+void func_808B0B8C(BgSpot08Iceblock* this);
+void func_808B0BA8(Vec3f* arg0, Vec3f* arg1, f32 arg2);
+void func_808B0BD4(Vec3f* arg0, Vec3f* arg1, Vec3f* arg2);
+s32 func_808B0C44(Vec3f* arg0, Vec3f* arg1);
+void func_808B0CE0(BgSpot08Iceblock* this, GlobalContext* globalCtx);
+void func_808B1054(BgSpot08Iceblock* this, GlobalContext* globalCtx);
+void func_808B1388(BgSpot08Iceblock* this);
+void func_808B13AC(BgSpot08Iceblock* this, GlobalContext* globalCtx);
+void func_808B13FC(BgSpot08Iceblock* this);
+void func_808B1420(BgSpot08Iceblock* this, GlobalContext* globalCtx);
+void func_808B147C(BgSpot08Iceblock* this);
+void func_808B14A0(BgSpot08Iceblock* this, GlobalContext* globalCtx);
+void func_808B1574(BgSpot08Iceblock* this);
+
+extern Gfx D_6000DE0[];
+extern CollisionHeader D_6001904;
+extern Gfx D_6002BD0[];
+extern CollisionHeader D_6002FD8;
 
 /*
 const ActorInit Bg_Spot08_Iceblock_InitVars = {
@@ -27,6 +46,11 @@ const ActorInit Bg_Spot08_Iceblock_InitVars = {
     (ActorFunc)BgSpot08Iceblock_Draw,
 };
 */
+
+extern Vec3f D_808B16BC;
+extern f32 D_808B16D4[3];
+extern f32 D_808B16E0[3];
+extern f32 D_808B16EC[3];
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Spot08_Iceblock/func_808B0960.s")
 
--- src/overlays/z_bg_spot08_iceblock.h
+++ src/overlays/z_bg_spot08_iceblock.h
@@ -7,9 +7,18 @@
 struct BgSpot08Iceblock;
 
 typedef struct BgSpot08Iceblock {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x50];
-} BgSpot08Iceblock; // size = 0x019C
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x164 */ void (*unk164)(struct BgSpot08Iceblock*, GlobalContext*);
+    /* 0x168 */ Vec3f unk168;
+    /* 0x174 */ Vec3f unk174;
+    /* 0x180 */ Vec3f unk180;
+    /* 0x18C */ s16 unk18C;
+    /* 0x18E */ s16 unk18E;
+    /* 0x190 */ s16 unk190;
+    /* 0x192 */ s16 unk192;
+    /* 0x194 */ f32 unk194;
+    /* 0x198 */ f32 unk198;
+} BgSpot08Iceblock; // size = 0x19C
 
 extern const ActorInit Bg_Spot08_Iceblock_InitVars;
 
