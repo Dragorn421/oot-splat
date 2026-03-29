1c4fc5bf827c0806cc4f79720448f6d756269bc6
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-29 12:58:27 +0200

    BgHakaHuta data

--- src/overlays/ovl_Bg_Haka_Huta.c
+++ src/overlays/ovl_Bg_Haka_Huta.c
@@ -20,7 +20,6 @@ void func_8087D8C0(BgHakaHuta* this, GlobalContext* globalCtx);
 extern Gfx D_60006B0[];
 extern CollisionHeader D_6000870;
 
-/*
 const ActorInit Bg_Haka_Huta_InitVars = {
     ACTOR_BG_HAKA_HUTA,
     ACTORCAT_BG,
@@ -32,7 +31,14 @@ const ActorInit Bg_Haka_Huta_InitVars = {
     (ActorFunc)BgHakaHuta_Update,
     (ActorFunc)BgHakaHuta_Draw,
 };
-*/
+static InitChainEntry D_8087D940[] = {
+    ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
+};
+static Vec3f D_8087D944 = { 0.0f, 0.0f, 0.0f };
+static Color_RGBA8 D_8087D950 = { 30, 20, 50, 255 };
+static Color_RGBA8 D_8087D954 = { 0, 0, 0, 255 };
+static Vec3f D_8087D958 = { 30.0f, 0.0f, 0.0f };
+static Vec3f D_8087D964 = { 0.03258f, 0.3258f, -0.9449f };
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Haka_Huta/BgHakaHuta_Init.s")
 
