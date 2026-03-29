0a9702bfb0b247ae3f3064c098dcccbaf70be897
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-03-29 07:43:29 +0200

    EnGe1 analysis

--- src/overlays/z_en_ge1.h
+++ src/overlays/z_en_ge1.h
@@ -7,9 +7,22 @@
 struct EnGe1;
 
 typedef struct EnGe1 {
-    /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x170];
-} EnGe1; // size = 0x02BC
+    /* 0x000 */ Actor actor;
+    /* 0x14C */ ColliderCylinder unk14C;
+    /* 0x198 */ SkelAnime unk198;
+    /* 0x1DC */ Vec3s unk1DC[16];
+    /* 0x23C */ Vec3s unk23C[16];
+    /* 0x29C */ Vec3s unk29C;
+    /* 0x2A2 */ Vec3s unk2A2;
+    /* 0x2A8 */ s16 unk2A8;
+    /* 0x2AA */ s16 unk2AA;
+    /* 0x2AC */ u16 unk2AC;
+    /* 0x2AE */ u8 unk2AE;
+    /* 0x2AF */ u8 unk2AF;
+    /* 0x2B0 */ AnimationHeader* unk2B0;
+    /* 0x2B4 */ void (*unk2B4)(struct EnGe1*, GlobalContext*);
+    /* 0x2B8 */ void (*unk2B8)(struct EnGe1*);
+} EnGe1; // size = 0x2BC
 
 extern const ActorInit En_Ge1_InitVars;
 
