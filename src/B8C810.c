cab63f6907ebcb6e0b381909eaf679323693164f
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-07-26 18:12:31 +0200

    more bss work and cleanup

--- src/B8C810.c
+++ src/B8C810.c
@@ -10,6 +10,9 @@ void* D_801D372C = NULL;
 void* D_801D3730 = NULL;
 void (*D_801D3734)(void*, void*, void*) = NULL;
 
+s32 D_801E17E0;
+s32 D_801E17E4;
+
 void func_801C91B0(s32 arg0) {
     D_801D3724 = func_801CA698(arg0);
     D_801D3728 = 1;
