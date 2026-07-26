98d955f2426f27e587c240c3859a18028dd8a2d6
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-07-26 06:15:15 +0200

    leoRezero

--- src/B92470.c
+++ src/B92470.c
@@ -1,3 +1,23 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/B92470/leoRezero.s")
+void leoRezero(void) {
+    u8 var_s0;
+    u8 temp_v0;
+
+    var_s0 = 8;
+    do {
+        temp_v0 = leoRecal_w();
+        if (temp_v0 == 0) {
+            LEOtgt_param.cylinder = 0;
+            LEOtgt_param.head = 0;
+            LEOtgt_param.zone = 0;
+            LEOcur_command->header.status = 0;
+            return;
+        }
+        if (leoChk_err_retry(temp_v0) != 0) {
+            break;
+        }
+    } while (var_s0--);
+    LEOcur_command->header.sense = temp_v0;
+    LEOcur_command->header.status = 2;
+}
