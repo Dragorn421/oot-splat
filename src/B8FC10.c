89ef8acaaace96b3ee53ad4347ed5ca8634f9e8b
inspectredc <78732756+inspectredc@users.noreply.github.com>
2026-07-26 06:42:54 +0200

    leoRead_common

--- src/B8FC10.c
+++ src/B8FC10.c
@@ -7,5 +7,55 @@ void leoRead(void) {
     leoRead_common(0x18U);
 }
 
-// https://decomp.me/scratch/L0MQ0
-#pragma GLOBAL_ASM("asm/nonmatchings/B8FC10/leoRead_common.s")
+void leoRead_common(unsigned int offset) {
+    u32 tg_lba;
+    u32 tg_blocks;
+    u32 message;
+    u16 retry_cnt;
+
+    tg_lba = LEOcur_command->data.readwrite.lba;
+    tg_blocks = LEOcur_command->data.readwrite.xfer_blks;
+    if ((tg_lba | tg_blocks) & 0xFFFF0000) {
+        goto invalid_lba;
+    }
+    tg_lba += offset;
+    if ((tg_lba + tg_blocks) >= 0x10DD) {
+    invalid_lba:
+        LEOcur_command->header.sense = 0x20;
+        LEOcur_command->header.status = 2;
+        return;
+    }
+    if (tg_blocks == 0) {
+        if (tg_lba >= 0x10DC) {
+            goto invalid_lba;
+        }
+        LEOcur_command->header.sense = 0;
+        LEOcur_command->header.status = 0;
+        return;
+    }
+    LEOtgt_param.lba = tg_lba;
+    LEOrw_flags &= ~0xC000;
+    osSendMesg(&LEOc2ctrl_que, NULL, 0);
+    osStartThread(&LEOinterruptThread);
+
+    while (1) {
+        osRecvMesg(&LEOcontrol_que, &message, 1);
+        switch (message) {
+            case 0x90000:
+                goto read_complete;
+            case 0x80000:
+                leoC2_Correction();
+                LEOrw_flags &= ~0x4000;
+                if (LEOcur_command) {}
+                osSendMesg(&LEOc2ctrl_que, NULL, 0);
+                break;
+            default:
+                LEOcur_command->header.sense = (u8)message;
+                LEOcur_command->header.status = 2;
+                return;
+        }
+    }
+read_complete:
+    LEOcur_command->header.sense = 0;
+    LEOcur_command->header.status = 0;
+}
