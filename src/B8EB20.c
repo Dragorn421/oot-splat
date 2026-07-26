014a88df27f2996eab71f93897ea34a93e9a0638
inspectredc <78732756+inspectredc@users.noreply.github.com>
2026-07-26 17:16:41 +0200

    leointerrupt, leoTranslate

--- src/B8EB20.c
+++ src/B8EB20.c
@@ -2,8 +2,47 @@
 
 extern vu16 LEOrw_flags;
 
-// https://decomp.me/scratch/n5d7V
-#pragma GLOBAL_ASM("asm/nonmatchings/B8EB20/leointerrupt.s")
+void leointerrupt(void* arg) {
+    u32 result;
+    u32 tg_blocks;
+
+    osCreateMesgQueue(&LEOc2ctrl_que, &LEOc2ctrl_que_buf, 1);
+
+    while (1) {
+        osStopThread(&LEOinterruptThread);
+        tg_blocks = LEOcur_command->data.readwrite.xfer_blks;
+        LEOwrite_pointer = LEOcur_command->data.readwrite.buff_ptr;
+
+        do {
+            leoLba_to_phys(LEOtgt_param.lba);
+            if (LEOrw_flags & 0x8000) {
+                result = leoSeek_i(1);
+            } else {
+                result = leoSeek_i(0);
+            }
+            if (result != 0) {
+                goto complete;
+            }
+            if (LEOrw_flags & 0x2000) {
+                LEOtgt_param.rdwr_blocks = 1;
+            } else if (LEOtgt_param.rdwr_blocks > tg_blocks) {
+                LEOtgt_param.rdwr_blocks = tg_blocks;
+            }
+            LEOtgt_param.lba += LEOtgt_param.rdwr_blocks;
+            tg_blocks -= LEOtgt_param.rdwr_blocks;
+            result = func_801CB650();
+            if (result != 0) {
+                goto complete;
+            }
+            LEOcur_command->data.readwrite.rw_bytes =
+                (uintptr_t)LEOwrite_pointer - (uintptr_t)LEOcur_command->data.readwrite.buff_ptr;
+        } while (tg_blocks != 0);
+        result = 0x90000;
+
+    complete:
+        osSendMesg(&LEOcontrol_que, result, 1);
+    }
+}
 
 u32 func_801CB650(void) {
     u32 message;
